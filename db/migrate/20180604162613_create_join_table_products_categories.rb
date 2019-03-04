8148e38b592d0dd1b44d458c9e2c38895362fbb3
stivendiaz <stivendiazagudelo@gmail.com>
2019-03-04 14:13:37 -0500

    ready

---
+++ db/migrate/20180604162613_create_join_table_products_categories.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableProductsCategories < ActiveRecord::Migration[5.1]
+  def change
+    create_join_table :products, :categories do |t|
+      t.references :product, foreign_key: true
+      t.references :category, foreign_key: true
+    end
+  end
+end
